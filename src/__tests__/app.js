5e8fd839ed3011c33a49d330d6a34283beed7258
Овчинников Игорь <faceplaces@inbox.ru>
2023-05-10 22:23:12 +0300

    first commit

--- src/__tests__/app.js
+++ src/__tests__/app.js
@@ -1,8 +1,36 @@
-import ArrayBufferConverter from '../app';
-import getBuffer from '../getbuffer';
+import read from '../reader';
+import GameSavingLoader from '../app';
 
-test('Testing ArrayBufferConverter', () => {
-  const converter = new ArrayBufferConverter(getBuffer());
-  const expected = '{"data":{"user":{"id":1,"name":"Hitman","level":10}}}';
-  expect(converter.toString()).toEqual(expected);
+jest.mock('../reader');
+beforeEach(() => {
+  jest.resetAllMocks();
+});
+
+test('Testing resolve', async () => {
+  const data = '{"id":9,"created":1546300800,"userInfo":{"id":1,"name":"Hitman","level":10,"points":2000}}';
+  const buffer = new ArrayBuffer(data.length * 2);
+  const bufferView = new Uint16Array(buffer);
+  for (let i = 0; i < data.length; i++) {
+    bufferView[i] = data.charCodeAt(i);
+  }
+  read.mockResolvedValue(buffer);
+  const result = await GameSavingLoader.load();
+  const date = new Date(1546300800);
+  expect(result).toEqual({
+    id: 9,
+    created: date.toISOString(),
+    userInfo: {
+      id: 1, name: 'Hitman', level: 10, points: 2000,
+    },
+  });
+});
+
+test('Testing reject', async () => {
+  expect.assertions(1);
+  read.mockResolvedValue(new ArrayBuffer(0));
+  try {
+    const result = await GameSavingLoader.load();
+  } catch (err) {
+    expect(err.message).toEqual('Wrong string format!');
+  }
 });
